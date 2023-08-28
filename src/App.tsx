f7336106abf07af95e120ccdd2a3d768b9c7a5f2
Bradley Black <bradleyblackdev@gmail.com>
2023-08-28 18:06:21 -0500

    mega template styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import './MegaTemplate.css';
 import Quiz from './Quix';
 import {
   BrowserRouter as Router,
--- src/TemplateA.tsx
+++ src/TemplateA.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './MegaTemplate.css';
 import Quiz from './Quix';
+import ReactMarkdown from "react-markdown";
 import {
     BrowserRouter as Router,
     Route,
@@ -8,34 +9,23 @@ import {
     Link,
 } from "react-router-dom";
 
-
 const TemplateA = () => {
-
 
     return (
         <div className="page">
-            <div className="head">
+            <div className="page-header">
                 <h1>wow</h1>
             </div>
-            <div className="arf">
+            <div className="page-body">
                 <p>item 1</p>
                 <p>item 2</p>
                 <p>item 3</p>
-                <p>item 4</p>
-                {/* <p>item 5</p>
-                <p>item 6</p>
-                <p>item 7</p>
-                <p>item 8</p>
-                <p>item 9</p>
-                <p>item 0</p>
-                <p>item 2</p>
-                <p>item 3</p>
-                <p>item 1</p>
-                <p>item 2</p>
-                <p>item 3</p> */}
+                <p><ReactMarkdown>item **44**</ReactMarkdown></p>
                 <div className="spacer" />
-                <p>footer!</p>
-
+                <div className="footer">
+                    <button className="back">Back</button>
+                    <button className="primary">Next</button>
+                </div>
             </div>
         </div>
         );
